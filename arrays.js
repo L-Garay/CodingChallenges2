b8b12a4e3b79392403df791863979722cc88e9d8
Logan Garay <garay.logan@gmail.com>
2020-11-25 14:25:25 -0700

    couple of array challenges

--- arrays.js
+++ arrays.js
@@ -44,3 +44,47 @@ export function canCapture([yourRook, opponentsRook]) {
 export function diffMaxMin(arr) {
   return Math.max(...arr) - Math.min(...arr);
 }
+
+// Find all prime numbers in an array
+export function primeNumbers(arr) {
+  let primeArr = arr.filter((number) => {
+    for (let i = 2; i < number; i++) {
+      if (number % i == 0) {
+        return false;
+      }
+      return true;
+    }
+  });
+  console.log(primeArr);
+}
+
+// Remove all duplicates from an array
+export function removeDuplicates(arr) {
+  let uniqueArr = [];
+  arr.forEach((el) => {
+    if (uniqueArr.includes(el) == false) {
+      uniqueArr.push(el);
+    }
+  });
+  console.log(uniqueArr);
+  return uniqueArr;
+}
+
+// Include only unique elements in array
+export function uniqueOnly(arr) {
+  let UniqueArr = removeDuplicates(arr);
+  let repeat = 0;
+  let reallyUnique = [];
+  UniqueArr.forEach((elem) => {
+    for (let i = 0; i < arr.length; i++) {
+      if (elem == arr[i]) {
+        repeat++;
+      }
+    }
+    if (repeat < 2) {
+      reallyUnique.push(elem);
+    }
+    repeat = 0;
+  });
+  console.log(reallyUnique);
+}
--- test.js
+++ test.js
@@ -1,5 +1,12 @@
 // NOTE Import different methods here
-import { maxItems, canCapture, diffMaxMin } from './arrays.js';
+import {
+  maxItems,
+  canCapture,
+  diffMaxMin,
+  primeNumbers,
+  removeDuplicates,
+  uniqueOnly,
+} from './arrays.js';
 import { isRepdigit, isAvgWhole } from './math.js';
 import { makeTitle, transformUpvotes } from './strings.js';
 
@@ -24,5 +31,12 @@ import { makeTitle, transformUpvotes } from './strings.js';
 // let str = '20.3k 3.8k 7.7k 992';
 // transformUpvotes(str);
 
-let arr = [4, 1, 7, 9, 2, 5, 7, 2, 4];
-isAvgWhole(arr);
+// let arr = [4, 1, 7, 9, 2, 5, 7, 2, 4];
+// isAvgWhole(arr);
+
+// let arr = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// primeNumbers(arr);
+
+let arr = [2, 3, 5, 7, 'n', 'f', 'w', 'v', 3, 5, 'n', 'v', 3, 5, 'x', 8, 7];
+// removeDuplicates(arr);
+uniqueOnly(arr);
